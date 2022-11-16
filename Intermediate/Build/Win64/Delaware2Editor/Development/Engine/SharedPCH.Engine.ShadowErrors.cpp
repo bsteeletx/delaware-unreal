0c68429d1d94bc9f5e73a5b1bd3306d02cc8e376
bsteele <bsteele@4846ca80-80c6-4f15-b73a-5698f23b0318>
2022-11-16 17:18:34 +0000

    Wrong SVN Folder Format

--- Intermediate/Build/Win64/Delaware2Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.33.31630
-#include "C:/Users/Admin/Documents/Unreal Projects/Delaware2/Intermediate/Build/Win64/Delaware2Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- Source/Delaware2/Delaware2.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Delaware2.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Delaware2, "Delaware2" );
-
-DEFINE_LOG_CATEGORY(LogDelaware2)
- 
